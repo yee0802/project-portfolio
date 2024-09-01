aaa629ebbbc542b47e4248b0eb101005db95993e
Kye <NovusAa12@proton.me>
2024-09-01 23:26:52 +0100

    feat: add UI components to Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
-import Image from "next/image";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-center py-12 px-4 md:px-8 lg:px-12"></main>
+    <main className="min-h-screen py-6 px-2 sm:px-4 md:px-8 lg:px-12">
+      <div className="w-full flex flex-col items-center">
+        <div className="max-w-6xl w-full lg:px-6">
+          <Header />
+          <Hero />
+        </div>
+
+        <div className="max-w-4xl w-full flex flex-col items-center opacity-[0.001] animate-fade-in">
+          <Skills />
+
+          <Projects />
+
+          <Footer />
+        </div>
+      </div>
+    </main>
   );
 }
