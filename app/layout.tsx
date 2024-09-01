12e19df67044f1ffddaf514fe303e7c07ae8bd63
Kye <NovusAa12@proton.me>
2024-09-01 23:55:57 +0100

    feat: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,22 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://kye-portfolio.vercel.app/"),
   title: "Kye Yee",
+  authors: {
+    name: "Kye Yee",
+  },
   description:
     "A Software Developer based in Bath, UK with expertise in both Frontend and Backend applications.",
+  openGraph: {
+    title: "Kye Yee",
+    description:
+      "A Software Developer based in Bath, UK with expertise in both Frontend and Backend applications.",
+    url: "https://kye-portfolio.vercel.app/",
+    siteName: "Kye Yee",
+    images: "/nextjs-portfolio.png",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
