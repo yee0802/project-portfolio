12a6c9cb540825929a7383fb320221056de525ee
Kye <NovusAa12@proton.me>
2025-01-04 16:03:39 +0000

    chore: update footer text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,9 @@ import SocialLinks from "./SocialLinks";
 export default function Footer() {
   return (
     <div className="flex flex-col items-center mt-8 w-full">
-      <span className="text-gray-300 font-normal text-base mb-4">2024</span>
+      <span className="text-gray-300 font-normal text-base mb-4">
+        Made with ❤️ by Kye
+      </span>
 
       <SocialLinks />
     </div>
